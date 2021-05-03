4095f170576730985aeb7470d2cf9c33454f4bfe
Noureddine-ait <nour-eddine.aitlahcen@edu.uca.ma>
2021-05-03 20:33:04 +0000

    noureddine commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { MenuComponent } from './view/menu/menu.component';
+import { PageNotFoundComponent } from './view/page-not-found/page-not-found.component';
+import {AppRoutingModule, components} from './app-routing.module';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    components,
+    MenuComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
